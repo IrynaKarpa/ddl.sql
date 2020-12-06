c08a462798a4cd9f789d7d9418f97acc5d762e31
VicktorBoiarchuk <72271816+VicktorBoiarchuk@users.noreply.github.com>
2020-12-06 20:27:43 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -6,6 +6,21 @@ create table customers (
   phone nvarchar(15) not null,
   address nvarchar(50) not null,
   birth date)
+  create table shops (  
+  id_shops INT not null PRIMARY KEY,
+  name VARCHAR(55) NOT NULL,
+  address VARCHAR(50) NOT NULL)
+create table orders (
+  id_order INT not null PRIMARY KEY,
+  date DATETIME NOT NULL)
+create table products (
+  id_prod INT not null PRIMARY KEY,
+  name VARCHAR(55) NOT NULL,
+  price NUMERIC NOT NULL)
+create table ingredients (
+  id_ingr INT not null PRIMARY KEY,
+  name NVARCHAR(55) NOT NULL,
+  price NUMERIC NOT NULL)
 create table head_office (
   id_head INT not null PRIMARY KEY,
   name VARCHAR(55) NOT NULL,
